3a8546e755963e4d6a55ed58d9e3341655a64bf1
Wei ren <41813453+k100956@users.noreply.github.com>
2018-09-17 19:14:56 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
 IMAGE=cc102_ecr_test
 #docker build -t $USERNAME/$IMAGE:mysql -f Chatbot_Dev/dockerfile-mysql Chatbot_Dev/
 #docker build -t $USERNAME/$IMAGE:api -f Chatbot_Dev/dockerfile-api Chatbot_Dev/
-docker build -t $USERNAME/$IMAGE:redis -f Chatbot_Line/dockerfile-redis Chatbot_Line/
-docker build -t $USERNAME/$IMAGE:jupyter -f Chatbot_Line/dockerfile-jupyter Chatbot_Line/
-docker build -t $USERNAME/$IMAGE:ngrok -f Chatbot_Line/dockerfile-ngrok Chatbot_Line/
+docker build -t $USERNAME/$IMAGE:redis -f dockerfile/dockerfile-redis dockerfile/
+docker build -t $USERNAME/$IMAGE:jupyter -f dockerfile/dockerfile-jupyter dockerfile/
+docker build -t $USERNAME/$IMAGE:ngrok -f dockerfile/dockerfile-ngrok dockerfile/
 
 
 #docker-compse up -d
